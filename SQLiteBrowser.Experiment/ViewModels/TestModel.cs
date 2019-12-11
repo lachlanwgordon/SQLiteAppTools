c81663a4845120ffe8ccf238b82555c397aaf001
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-12-11 20:50:52 +1100

    experimenting

---
+++ SQLiteBrowser.Experiment/ViewModels/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SQLiteBrowser.Experiment.ViewModels
+{
+    public class TestModel : ContentPage
+    {
+        public TestModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+

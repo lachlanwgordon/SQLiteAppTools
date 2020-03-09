427aa6a210418cb2fb674bd68a34841e98e25bbe
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-03-09 13:03:33 +1100

    Added some loading performance monitoring Made a reasonable layout instead of the wacky colours and bad margins

--- SQLiteBrowser/ViewModels/AltBrowserViewModel.cs
+++ SQLiteBrowser/ViewModels/AltBrowserViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MvvmHelpers;
@@ -17,13 +19,26 @@ namespace SQLiteBrowser.ViewModels
         }
         public static string Path { get; set; }
 
+        public static Stopwatch Stopwatch = new Stopwatch();
         public async Task OnAppearingAsync()
         {
+            Stopwatch.Reset();
+            Stopwatch.Start();
+
+
+            Debug.WriteLine($"On appearing {Stopwatch.ElapsedMilliseconds}ms   memory: {GC.GetTotalMemory(false)}");
             var connection = new SQLite.SQLiteConnection(Path);
-            
+            Debug.WriteLine($"Connected {Stopwatch.ElapsedMilliseconds}ms   memory: {GC.GetTotalMemory(false)}");
+
             var tables = Table.GetAll(connection);
+            Debug.WriteLine($"Loaded {Stopwatch.ElapsedMilliseconds}ms   memory: {GC.GetTotalMemory(false)}");
+
             Tables.AddRange(tables);
+            Debug.WriteLine($"added {Stopwatch.ElapsedMilliseconds}ms   memory: {GC.GetTotalMemory(false)}");
+
             SelectedTable = tables.FirstOrDefault();
+            Debug.WriteLine($"selected {Stopwatch.ElapsedMilliseconds}ms   memory: {GC.GetTotalMemory(false)}");
+
         }
     }
 }

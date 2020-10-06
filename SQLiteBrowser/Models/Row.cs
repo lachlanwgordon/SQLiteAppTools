818ac1a187dc7ce8ef083e135aa7c242b7330114
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-10-06 16:55:27 +1100

    Refactor to remove dependency on MVVM Helpers.
    I love them but want to minimise nuget conflicts for users
    
    Also deleted some dead code

--- SQLiteBrowser/Models/Row.cs
+++ SQLiteBrowser/Models/Row.cs
@@ -8,7 +8,6 @@ namespace SQLiteBrowser.Models
 {
     public class Row
     {
-        public Double Width => Cells.Count * 100;
         List<ColumnInfo> ColumnInfos;
         public List<Cell> Cells { get; set; } = new List<Cell>();
         private IEnumerable<object> items;
@@ -47,26 +46,5 @@ namespace SQLiteBrowser.Models
                 index++;
             }
         }
-
-        public Cell Cell0 => Cells.ElementAtOrDefault(0) ?? BlankCell;
-        public Cell Cell1 => Cells.ElementAtOrDefault(1) ?? BlankCell;
-        public Cell Cell2 => Cells.ElementAtOrDefault(2) ?? BlankCell;
-        public Cell Cell3 => Cells.ElementAtOrDefault(3) ?? BlankCell;
-        public Cell Cell4 => Cells.ElementAtOrDefault(4) ?? BlankCell;
-        public Cell Cell5 => Cells.ElementAtOrDefault(5) ?? BlankCell;
-        public Cell Cell6 => Cells.ElementAtOrDefault(6) ?? BlankCell;
-        public Cell Cell7 => Cells.ElementAtOrDefault(7) ?? BlankCell;
-        public Cell Cell8 => Cells.ElementAtOrDefault(8) ?? BlankCell;
-        public Cell Cell9 => Cells.ElementAtOrDefault(9) ?? BlankCell;
-        public Cell Cell10 => Cells.ElementAtOrDefault(10) ?? BlankCell;
-        public Cell Cell11 => Cells.ElementAtOrDefault(11) ?? BlankCell;
-        public Cell Cell12 => Cells.ElementAtOrDefault(12) ?? BlankCell;
-        public Cell Cell13 => Cells.ElementAtOrDefault(13) ?? BlankCell;
-        public Cell Cell14 => Cells.ElementAtOrDefault(14) ?? BlankCell;
-        public Cell Cell15 => Cells.ElementAtOrDefault(15) ?? BlankCell;
-        public Cell Cell16 => Cells.ElementAtOrDefault(16) ?? BlankCell;
-        public Cell Cell17 => Cells.ElementAtOrDefault(17) ?? BlankCell;
-
-        public Cell BlankCell = new Cell(null);
     }
 }
